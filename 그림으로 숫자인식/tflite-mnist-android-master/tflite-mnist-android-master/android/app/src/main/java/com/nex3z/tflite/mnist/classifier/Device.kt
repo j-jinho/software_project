879ec0175d0c084379809921368a521df8f6c030
kwonhyeokgeun <49871871+kwonhyeokgeun@users.noreply.github.com>
2021-01-14 20:26:33 +0900

    Delete 그림으로 숫자인식 directory

--- 그림으로 숫자인식/tflite-mnist-android-master/tflite-mnist-android-master/android/app/src/main/java/com/nex3z/tflite/mnist/classifier/Device.kt
+++
@@ -1,7 +0,0 @@
-package com.nex3z.tflite.mnist.classifier
-
-enum class Device {
-    CPU,
-    NNAPI,
-    GPU
-}
--- 그림으로 숫자인식/tflite-mnist-android-master/tflite-mnist-android-master/android/app/src/main/java/com/nex3z/tflite/mnist/classifier/Recognition.kt
+++
@@ -1,7 +0,0 @@
-package com.nex3z.tflite.mnist.classifier
-
-data class Recognition(
-    val label: Int,
-    val confidence: Float,
-    val timeCost: Long
-)
